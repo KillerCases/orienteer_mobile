39ce352184663a83b0fc8c298728c1866a04dec5
Jo Wightman <jo.killercases@gmail.com>
2014-02-08 22:40:46 -0500

    updated to version 3.3 of Phonegap

---
+++ platforms/android/CordovaLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jwightman/Dropbox/orienteer_mobile/orienteer.it/platforms/android/CordovaLib/bin/AndroidManifest.xml : \
+/Users/jwightman/Dropbox/orienteer_mobile/orienteer.it/platforms/android/CordovaLib/AndroidManifest.xml \
